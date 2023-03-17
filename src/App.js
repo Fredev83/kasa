0667504030b8aebc1aeafa213bce9032a8ef61ce
Frédéric Rouxel <fredericrouxel@macbook-air-de-frederic.home>
2023-03-17 10:15:06 +0100

    creation des dossiers dans src

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Hello world !
+          Hello world welcome !
         </h1>
         <a
           className="App-link"
