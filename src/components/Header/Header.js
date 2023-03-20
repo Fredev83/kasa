b89e1ab1b87165675ea492be0196e1a08af3b9a0
Frédéric Rouxel <fredericrouxel@macbook-air-de-frederic.home>
2023-03-20 15:01:17 +0100

    fix sass

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,12 @@ import logo from "../../assets/images/LOGO.svg";
 import Nav from "../Nav/Nav";
 
 export default function Header() {
-    return (
-        <header className="headerWrap">
-            <figure className="headerWrap__fig">
-                <img className="logo" src={logo} alt="logo de l'agence kasa" />
-            </figure>
-            <Nav className="nav-header" />
-        </header>
-    );
+	return (
+		<header className="headerWrap">
+			<figure className="headerWrap__fig">
+				<img className="logo" src={logo} alt="logo de l'agence kasa" />
+			</figure>
+			<Nav className="nav-header" />
+		</header>
+	);
 }
