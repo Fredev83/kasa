b13303af229902809188572f0531665573cf5592
Frédéric Rouxel <fredericrouxel@macbook-air-de-frederic.home>
2023-03-21 11:18:10 +0100

    fix bug node_module => npm reinstall

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
+import Home from "./pages/Home/Home2";
 import About from "./pages/About/About";
 import Header from "./components/Header/Header";
 import Logement from "./pages/Logement/Logement";
